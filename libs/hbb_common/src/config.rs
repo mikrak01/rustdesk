9afcb2fec6c8b6a538e65c6cd7b7a97d024ea52a
mikrak01 <166975320+mikrak01@users.noreply.github.com>
2024-09-21 16:58:28 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["pxy.4247701.xyz"];
-pub const PUBLIC_RS_PUB_KEY: &str = "EHzfjZZ2TeT+HCjFHDuJHFyJD15XRbzjhK7h4kYo4os=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.goodtimektv.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "LdSVacyXzlWooEwg6SLkwNQ+77gTT8JsfRVfDpxWU+c=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
